c1be4e5c8a9ca09c813074fdadcac46d3bc4a8f8
Daniel Buchan <clone@myself.com>
2024-03-11 15:45:02 +0000

    added morecambe scripts

--- scripts/run_pfam.sh
+++ scripts/run_pfam.sh
@@ -4,11 +4,7 @@
 #$ -l h_rt=48:0:0
 #$ -j y
 #$ -N pfam_nw
+#$ -t 1-50
 
 # Run the application.
-module load python3
-source /home/ucbcdwb/Scratch/profile_drift/profile_drift/bin/activate
-mkdir $SGE_TASK_ID
-cd $SGE_TASK_ID/
-echo "python run_pfam_rep_blasts.py ~/Scratch/Data/pfam/reps.fasta.fa ~/Scratch/Data/pfam/pfam_fasta.fa $SGE_TASK_ID"
-python /home/ucbcdwb/Scratch/profile_drift/run_pfam_rep_blasts.py ~/Scratch/Data/pfam/reps.fasta.fa ~/Scratch/Data/pfam/pfam_fasta.fa $SGE_TASK_ID
+/home/dbuchan/profile_drift_new/scripts/pfam_reps_nw.py /home/dbuchan/pfam_nw/pfam_consensus_reps_labelled_flattened.fa /home/dbuchan/pfam_nw/${SGE_TASK_ID}_pfam_consensus > /home/dbuchan/pfam_nw/${SGE_TASK_ID}_hits.csv
