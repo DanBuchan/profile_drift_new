104634738976e108b4687eff3b3b8ce24aeb2f1c
Daniel Buchan <clone@myself.com>
2024-03-11 17:57:28 +0000

    things

--- scripts/run_pfam.sh
+++ scripts/run_pfam.sh
@@ -8,8 +8,8 @@
 
 # Run the application.
 echo 'python /home/dbuchan/profile_drift_new/scripts/pfam_reps_nw.py /home/dbuchan/pfam_nw/pfam_consensus_reps_labelled_flattened.fa /home/dbuchan/pfam_nw/${SGE_TASK_ID}_pfam_consensus > /home/dbuchan/pfam_nw/${SGE_TASK_ID}_hits.csv 2> /home/dbuchan/pfam_nw/${SGE_TASK_ID}_hits.err'
-mkdir /scratch0/pfam_nw/
-cd /scratch0/pfam_nw/
+mkdir /scratch0/pfam_nw_${SGE_TASK_ID}/
+cd /scratch0/pfam_nw_${SGE_TASK_ID}/
 python /home/dbuchan/profile_drift_new/scripts/pfam_reps_nw.py /home/dbuchan/pfam_nw/pfam_consensus_reps_labelled_flattened.fa /home/dbuchan/pfam_nw/${SGE_TASK_ID}_pfam_consensus > /home/dbuchan/pfam_nw/${SGE_TASK_ID}_hits.csv 2> /home/dbuchan/pfam_nw/${SGE_TASK_ID}_hits.err
 cd ~/
-rm -rf /scratch0/pfam_nw
+rm -rf /scratch0/pfam_nw_${SGE_TASK_ID}
