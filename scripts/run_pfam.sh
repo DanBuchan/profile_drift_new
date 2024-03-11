daf53f87ed41bf398af0e821433734d4b26c6d5a
Daniel Buchan <clone@myself.com>
2024-03-11 16:51:40 +0000

    added morecambe scripts

--- scripts/run_pfam.sh
+++ scripts/run_pfam.sh
@@ -7,4 +7,4 @@
 #$ -t 1-50
 
 # Run the application.
-/home/dbuchan/profile_drift_new/scripts/pfam_reps_nw.py /home/dbuchan/pfam_nw/pfam_consensus_reps_labelled_flattened.fa /home/dbuchan/pfam_nw/${SGE_TASK_ID}_pfam_consensus > /home/dbuchan/pfam_nw/${SGE_TASK_ID}_hits.csv
+/home/dbuchan/profile_drift_new/scripts/pfam_reps_nw.py /home/dbuchan/pfam_nw/pfam_consensus_reps_labelled_flattened.fa /home/dbuchan/pfam_nw/${SGE_TASK_ID}_pfam_consensus > /home/dbuchan/pfam_nw/${SGE_TASK_ID}_hits.csv 2> /home/dbuchan/pfam_nw/${SGE_TASK_ID}_hits.err
