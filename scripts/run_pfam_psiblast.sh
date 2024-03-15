f71e5cd2f6a4b40b5b508db5d9019e05978c8a30
Daniel Buchan <clone@myself.com>
2024-03-15 17:16:48 +0000

    minor shell script change

--- scripts/run_pfam_psiblast.sh
+++ scripts/run_pfam_psiblast.sh
@@ -24,7 +24,7 @@
 
 # Run the application.
 module load python3
-source /home/ucbcdwb/Scratch/profile_drift/profile_drift/bin/activate
+source /home/ucbcdwb/Scratch/virtualenvs/profile_drift/bin/activate
 mkdir $SGE_TASK_ID
 cd $SGE_TASK_ID/
 echo "python /home/ucbcdwb/Applications/profile_drift_new/scripts/run_pfam_rep_blasts.py ~/Scratch/Data/pfam/pfam_consensus_reps_labelled_flattened.fa ~/Scratch/Data/pfam/Pfam-A.full.uniprot.fa $SGE_TASK_ID"
