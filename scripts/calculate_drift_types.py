42d7d7809851ee15e8d7d8149c52ce744b6f4212
Daniel Buchan <clone@myself.com>
2024-03-27 16:34:06 +0000

    drift_types update

--- scripts/calculate_drift_types.py
+++ scripts/calculate_drift_types.py
@@ -215,7 +215,7 @@ for family in significant_drifts:
     count+=1
     data = full_results[family]
     # print(family)
-    pprint.pp(data)
+    # pprint.pp(data)
     if data['families_at_final_iteration'] < data['number_of_drift_familes'] + 1:
         families_lost += 1
     growth_data = data['growth_types']
