2b04b670eb696638cacfcd92be914dc9b74e260c
Daniel Buchan <clone@myself.com>
2024-03-27 12:37:37 +0000

    drift_types update

--- scripts/calculate_drift_types.py
+++ scripts/calculate_drift_types.py
@@ -106,7 +106,7 @@ def calculate_drift_types(main_family, summary):
                         track_data[family]['peak_value'] = summary[iteration][family]
                         track_data[family]['peak_iteration'] = iteration
                                   
-    pprint.pp(track_data)
+    # pprint.pp(track_data)
     # Now work out drift types
     number_of_contaminents = len(track_data)-1
     results = {'number_of_drift_familes': number_of_contaminents,
@@ -141,7 +141,7 @@ for file in glob.glob(f'{summaries_dir}/*.csv'):
         non_drift_families.append(current_family)
     break
 
-print(full_results)
+pprint.pp(full_results)
 # print("drifts:", drift_families)
 # print("non drifts:", non_drift_families)
 # print("errors:", erroneous_files)
