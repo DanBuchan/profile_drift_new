12c3ec83f7f1202c5aff4c930992af4cd46ccacd
Daniel Buchan <clone@myself.com>
2024-04-24 14:57:05 +0100

    select script

--- scripts/psiblast_drift/select_targets.py
+++ scripts/psiblast_drift/select_targets.py
@@ -8,6 +8,7 @@ def select_from_list(file, number):
             print(pf_family)
             target_list.append(pf_family)
     if len(target_list) < number:
+        print("hi")
         return target_list
     return random.sample(target_list, number)
     
@@ -15,6 +16,7 @@ def select_from_list(file, number):
 print("family,type")
 drift_list = "results_data/drift_summary/non_drift_list.txt"
 drift_set = select_from_list(drift_list, 100)
+print(len(drift_set))
 for item in drift_set:
     print(f"{item},non_drift")
 
