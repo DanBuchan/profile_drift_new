04ec507e6b71595c0bc5897cf13edea78c1ce8ed
Daniel Buchan <clone@myself.com>
2024-04-24 14:57:14 +0100

    select script

--- scripts/psiblast_drift/select_targets.py
+++ scripts/psiblast_drift/select_targets.py
@@ -18,7 +18,8 @@ drift_list = "results_data/drift_summary/non_drift_list.txt"
 drift_set = select_from_list(drift_list, 100)
 print(len(drift_set))
 for item in drift_set:
-    print(f"{item},non_drift")
+    pass
+#     print(f"{item},non_drift")
 
 # drift_list = "results_data/drift_summary/non_drift_list.txt"
 # drift_set = select_from_list(drift_list, 100)
