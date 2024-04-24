7bd2fa93d5c4cb8de39f086bf0af5497a925199d
Daniel Buchan <clone@myself.com>
2024-04-24 15:00:07 +0100

    select script

--- scripts/psiblast_drift/select_targets.py
+++ scripts/psiblast_drift/select_targets.py
@@ -25,10 +25,10 @@ drift_set = select_from_list(drift_list, 100)
 for item in drift_set:
     print(f"{item},insig_drift")
 
-# drift_list = "results_data/drift_summary/set_where_contaminants_grew.txt"
-# drift_set = select_from_list(drift_list, 100)
-# for item in drift_set:
-#     print(f"{item},contaminants_grew")
+drift_list = "results_data/drift_summary/set_where_contaminants_grew.txt"
+drift_set = select_from_list(drift_list, 100)
+for item in drift_set:
+    print(f"{item},contaminants_grew")
 
 # drift_list = "results_data/drift_summary/set_where_contaminants_are_purified_out.txt"
 # drift_set = select_from_list(drift_list, 100)
