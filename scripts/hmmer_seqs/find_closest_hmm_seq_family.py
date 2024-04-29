2c95dba239fcf20967bc605988845f51f02dc952
Daniel Buchan <clone@myself.com>
2024-04-29 12:04:23 +0100

    hmm things

--- scripts/hmmer_seqs/find_closest_hmm_seq_family.py
+++ scripts/hmmer_seqs/find_closest_hmm_seq_family.py
@@ -4,6 +4,8 @@ import re
 import pprint
 import glob
 
+# usage: python scripts/hmmer_seqs/find_closest_hmm_seq_family.py results_data/hmmer_matches/hmm_generated_seqs.fa results_data/psiblast_iteration_summaries/
+
 # 2. Open MSA summary for family and get all families
 # 3. make fasta db of all the families
 # 4. Iteratr over seqs in family seeing what the top hits are
@@ -42,8 +44,15 @@ def get_pfam_family_names(seqs):
                 family_names.add(match.groups()[0])
     return list(family_names)
 
+def collect_all_fasta_seqs(pfam_family_names, summaries)
+    for family_name in pfam_family_names:
+        for file in glob.glob(f'*_{family_name}_*.csv'):
+            print(file)
+
 # 1. open file of generated seqs, read in and get family ID etc
 generated_seqs = get_hmm_generated_sequnces(sys.argv[1])
 # 2. get a unique list of the pfam family names
 pfam_family_names = get_pfam_family_names(generated_seqs)
+# 3. Collect a fasta file of all the seqs we are going to need
+collect_all_fasta_seqs(pfam_family_names, sys.argv[2])
 pprint.pp(pfam_family_names)
