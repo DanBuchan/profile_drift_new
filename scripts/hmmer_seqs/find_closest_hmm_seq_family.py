df533d10b999e1b8ec2d8afaa2d59f927e68c6ce
Daniel Buchan <clone@myself.com>
2024-04-29 17:38:51 +0100

    hmm things

--- scripts/hmmer_seqs/find_closest_hmm_seq_family.py
+++ scripts/hmmer_seqs/find_closest_hmm_seq_family.py
@@ -33,58 +33,58 @@ def get_hmm_generated_sequences(hmm_seqs):
     return seqs       
 
 def find_closest_fasta(all_family_seqs, gen_seqs, i):
-    with gen_seqs[i] as seq_record:      
-        match = re.search("^>.+\|(PF\d+-sample\d+)", seq_record['header'])
-        query_name = ''
-        if match:
-            query_name = match.groups()[0]
-        fhtmp = open(f"{query_name}.fa", "w", encoding="utf-8")
-        fhtmp.write(f'{seq_record["header"]}\n')
-        fhtmp.write(f'{seq_record["seq"]}\n')
-        fhtmp.close()
-        args = ['/home/dbuchan/Applications/fasta36/bin/fasta36',
-                '-q',
-                '-p',
-                '-O',
-                'out',
-                f"{query_name}.fa", 
-                f"{all_family_seqs}",
-        ]
-        print("Calculating", " ".join(args))
-        start = time.time()
-        try:
-            p = Popen(args, stdout=PIPE, stderr=PIPE)
-            result_stdout, err = p.communicate()
-        except Exception as e:
-            print(str(e))
-            sys.exit(1)
-        if p.returncode != 0:
-            print("Non Zero Exit status: "+str(p.returncode))
-            raise OSError("Non Zero Exit status: "+str(p.returncode))
-        results = result_stdout.decode('utf-8')
-        lines = results.split("\n")
-        parse_results = False
-        best_hit = 'None'
-        best_score = 'NA'
-        end = time.time()
-        print(f'RUN TIME: {end - start}')
-        for line in lines:
-            if parse_results:
-                entries = line.split()
-                best_hit = entries[0]
-                try:
-                    best_hit = best_hit.split("___")[1]
-                    best_score = line[62:]
-                    best_score = float(best_score.split()[0])
-                except:
-                    best_hit = 'None'
-                    best_score = 'NA'
-                break
-            if line.startswith("The best scores are:"):
-                parse_results = True
-            if "residues in 1 query   sequences" in line:
-                parse_results = False
-        print(query_name, best_hit, best_score)
+    seq_record = gen_seqs[i]:      
+    match = re.search("^>.+\|(PF\d+-sample\d+)", seq_record['header'])
+    query_name = ''
+    if match:
+        query_name = match.groups()[0]
+    fhtmp = open(f"{query_name}.fa", "w", encoding="utf-8")
+    fhtmp.write(f'{seq_record["header"]}\n')
+    fhtmp.write(f'{seq_record["seq"]}\n')
+    fhtmp.close()
+    args = ['/home/dbuchan/Applications/fasta36/bin/fasta36',
+            '-q',
+            '-p',
+            '-O',
+            'out',
+            f"{query_name}.fa", 
+            f"{all_family_seqs}",
+    ]
+    print("Calculating", " ".join(args))
+    start = time.time()
+    try:
+        p = Popen(args, stdout=PIPE, stderr=PIPE)
+        result_stdout, err = p.communicate()
+    except Exception as e:
+        print(str(e))
+        sys.exit(1)
+    if p.returncode != 0:
+        print("Non Zero Exit status: "+str(p.returncode))
+        raise OSError("Non Zero Exit status: "+str(p.returncode))
+    results = result_stdout.decode('utf-8')
+    lines = results.split("\n")
+    parse_results = False
+    best_hit = 'None'
+    best_score = 'NA'
+    end = time.time()
+    print(f'RUN TIME: {end - start}')
+    for line in lines:
+        if parse_results:
+            entries = line.split()
+            best_hit = entries[0]
+            try:
+                best_hit = best_hit.split("___")[1]
+                best_score = line[62:]
+                best_score = float(best_score.split()[0])
+            except:
+                best_hit = 'None'
+                best_score = 'NA'
+            break
+        if line.startswith("The best scores are:"):
+            parse_results = True
+        if "residues in 1 query   sequences" in line:
+            parse_results = False
+    print(query_name, best_hit, best_score)
 
 # 1. open file of generated seqs, read in and get family ID etc
 generated_seqs = get_hmm_generated_sequences(sys.argv[1])
