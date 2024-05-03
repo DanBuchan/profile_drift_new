5cfb7dadfda4dbd53027eef968605a6719fa3a97
Daniel Buchan <clone@myself.com>
2024-05-03 11:32:14 +0100

    small path changed

--- scripts/rep_distance_matrix/run_pfam_nw.sh
+++ scripts/rep_distance_matrix/run_pfam_nw.sh
@@ -7,9 +7,9 @@
 #$ -t 1-496
 
 # Run the application.
-echo 'python /home/dbuchan/profile_drift_new/scripts/pfam_reps_nw.py /home/dbuchan/pfam_nw/pfam_consensus_reps_labelled_flattened.fa /home/dbuchan/pfam_nw/${SGE_TASK_ID}_pfam_consensus > /home/dbuchan/pfam_nw/${SGE_TASK_ID}_hits.csv 2> /home/dbuchan/pfam_nw/${SGE_TASK_ID}_hits.err'
+echo 'python /home/dbuchan/profile_drift_new/scripts/pfam_reps_nw.py /home/dbuchan/pfam_nw/random_pfam_reps.fa /home/dbuchan/pfam_nw/${SGE_TASK_ID}_pfam_consensus > /home/dbuchan/pfam_nw/${SGE_TASK_ID}_hits.csv 2> /home/dbuchan/pfam_nw/${SGE_TASK_ID}_hits.err'
 mkdir /scratch0/pfam_nw_${SGE_TASK_ID}/
 cd /scratch0/pfam_nw_${SGE_TASK_ID}/
-python /home/dbuchan/profile_drift_new/scripts/pfam_reps_nw.py /home/dbuchan/pfam_nw/pfam_consensus_reps_labelled_flattened.fa /home/dbuchan/pfam_nw/${SGE_TASK_ID}_pfam_consensus > /home/dbuchan/pfam_nw/${SGE_TASK_ID}_hits.csv 2> /home/dbuchan/pfam_nw/${SGE_TASK_ID}_hits.err
+python /home/dbuchan/profile_drift_new/scripts/pfam_reps_nw.py /home/dbuchan/pfam_nw/random_pfam_reps.fa /home/dbuchan/pfam_nw/${SGE_TASK_ID}_pfam_consensus > /home/dbuchan/pfam_nw/${SGE_TASK_ID}_hits.csv 2> /home/dbuchan/pfam_nw/${SGE_TASK_ID}_hits.err
 cd ~/
 rm -rf /scratch0/pfam_nw_${SGE_TASK_ID}
