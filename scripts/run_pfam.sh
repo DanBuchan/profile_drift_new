043a3f96452d07d013b88b26e4620f67561b2c33
DanBuchan <clone@myself.com>
2024-05-03 11:15:37 +0100

    things

--- scripts/run_pfam.sh
+++ scripts/run_pfam.sh
@@ -4,7 +4,7 @@
 #$ -l h_rt=48:0:0
 #$ -j y
 #$ -N pfam_nw
-#$ -t 1-50
+##$ -t 1-496
 
 # Run the application.
 echo 'python /home/dbuchan/profile_drift_new/scripts/pfam_reps_nw.py /home/dbuchan/pfam_nw/pfam_consensus_reps_labelled_flattened.fa /home/dbuchan/pfam_nw/${SGE_TASK_ID}_pfam_consensus > /home/dbuchan/pfam_nw/${SGE_TASK_ID}_hits.csv 2> /home/dbuchan/pfam_nw/${SGE_TASK_ID}_hits.err'
