0ff49e68c67918809206ad5fcd5ff51754863904
DanBuchan <clone@myself.com>
2024-05-14 14:06:03 +0100

    thigns

--- scripts/hmmer_seqs/run_hmm_closest.sh
+++ scripts/hmmer_seqs/run_hmm_closest.sh
@@ -1,11 +1,11 @@
 #$ -S /bin/bash
-#$ -l tmem=1G
-#$ -l h_vmem=2G
-#$ -l h_rt=48:0:0
+#$ -l tmem=8G
+#$ -l h_vmem=8G
+#$ -l h_rt=1:0:0
 #$ -j y
 #$ -N hmm_closest
 ##$ -t 1-51900
-#$ -t 18-18
+#$ -t 1-100
 
 # Run the application.
 
@@ -13,7 +13,7 @@ mkdir /scratch0/pfam_nw_${SGE_TASK_ID}/
 cd /scratch0/pfam_nw_${SGE_TASK_ID}/
 
 echo 'python scripts/hmmer_seqs/find_closest_hmm_seq_family.py /home/dbuchan/inputs/hmm_generated_seqs.fa /home/dbuchan/inputs/Pfam-A.full.uniprot.fa /home/dbuchan/outputs/hmm_closest/ ${SGE_TASK_ID}'
-python /home/dbuchan/profile_drift_new/scripts/hmmer_seqs/find_closest_hmm_seq_family.py /home/dbuchan/inputs/hmm_generated_seqs.fa /home/dbuchain/inputs/Pfam-A.full.uniprot.fa /home/dbuchan/outputs/hmm_closest/ ${SGE_TASK_ID}
+python /home/dbuchan/profile_drift_new/scripts/hmmer_seqs/find_closest_hmm_seq_family.py /home/dbuchan/inputs/hmm_generated_seqs.fa /home/dbuchan/inputs/Pfam-A.full.uniprot.fa /home/dbuchan/outputs/hmm_closest/ ${SGE_TASK_ID}
 
 cd ~/
 rm -rf /scratch0/pfam_nw_${SGE_TASK_ID}
