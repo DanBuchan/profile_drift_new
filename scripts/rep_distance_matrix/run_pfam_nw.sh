3a3e06b91a28a4aeca67935e2e907c8c9e6843d6
Daniel Buchan <clone@myself.com>
2024-05-29 11:27:18 +0100

    tracking down dist mat bug

--- scripts/rep_distance_matrix/run_pfam_nw.sh
+++ scripts/rep_distance_matrix/run_pfam_nw.sh
@@ -7,9 +7,9 @@
 ##$ -t 62-62
 
 # Run the application.
-echo 'python /home/dbuchan/profile_drift_new/scripts/rep_distance_matrix/pfam_reps_nw.py /home/dbuchan/pfam_nw/random_pfam_reps.fa /home/dbuchan/pfam_nw/${SGE_TASK_ID}_pfam_consensus > /home/dbuchan/pfam_nw/${SGE_TASK_ID}_hits.csv 2> /home/dbuchan/pfam_nw/${SGE_TASK_ID}_hits.err'
+echo 'python /home/dbuchan/profile_drift_new/scripts/rep_distance_matrix/pfam_reps_nw.py /home/dbuchan/pfam_nw/random_pfam_reps.fa /home/dbuchan/pfam_nw/${SGE_TASK_ID}_pfam_random > /home/dbuchan/pfam_nw/${SGE_TASK_ID}_hits.csv 2> /home/dbuchan/pfam_nw/${SGE_TASK_ID}_hits.err'
 mkdir /scratch0/pfam_nw_${SGE_TASK_ID}/
 cd /scratch0/pfam_nw_${SGE_TASK_ID}/
-python /home/dbuchan/profile_drift_new/scripts/rep_distance_matrix/pfam_reps_nw.py /home/dbuchan/pfam_nw/random_pfam_reps.fa /home/dbuchan/pfam_nw/${SGE_TASK_ID}_pfam_consensus > /home/dbuchan/pfam_nw/${SGE_TASK_ID}_hits.csv 2> /home/dbuchan/pfam_nw/${SGE_TASK_ID}_hits.err
+python /home/dbuchan/profile_drift_new/scripts/rep_distance_matrix/pfam_reps_nw.py /home/dbuchan/pfam_nw/random_pfam_reps.fa /home/dbuchan/pfam_nw/${SGE_TASK_ID}_pfam_random > /home/dbuchan/pfam_nw/${SGE_TASK_ID}_hits.csv 2> /home/dbuchan/pfam_nw/${SGE_TASK_ID}_hits.err
 cd ~/
 rm -rf /scratch0/pfam_nw_${SGE_TASK_ID}
