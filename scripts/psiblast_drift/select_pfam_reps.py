1604731a7e4fae4ffc04cbee484b572759ed3a74
Daniel Buchan <clone@myself.com>
2024-07-01 13:09:42 +0100

    update select_targets

--- scripts/psiblast_drift/select_pfam_reps.py
+++ scripts/psiblast_drift/select_pfam_reps.py
@@ -22,13 +22,13 @@ with open(pfam_fa, "r", encoding="utf-8") as fhIn:
                 family_seqs.append({'header': header, 'seq': seq})
                 if this_family not in current_family:
                     current_family = this_family
-                    print(family_seqs)
+                    ## print(family_seqs)
                     rand_rep = random.choice(family_seqs)
                     # print(rand_rep)
                     print(rand_rep['header'])
                     print(rand_rep['seq'])
-                    family_reps = []
-                    exit()
+                    family_seqs = []
+                    ##exit()
             else:
                 current_family = this_family
             seq = ''
