2a0c941f0566d81ddbb86f7e0e725a8d75dc39fd
Daniel Buchan <clone@myself.com>
2024-07-27 11:48:08 +0100

    minor changes

--- scripts/hmmer_seqs/run_hmm_closest.sh
+++ scripts/hmmer_seqs/run_hmm_closest.sh
@@ -22,7 +22,7 @@ module load python3
 mkdir /home/ucbcdwb/Scratch/output/hmm/hmm_fa_${SGE_TASK_ID}/
 cd /home/ucbcdwb/Scratch/output/hmm/hmm_fa_${SGE_TASK_ID}/
 
-echo 'python scripts/hmmer_seqs/find_closest_hmm_seq_family.py /home/dbuchan/inputs/hmm_generated_seqs.fa /home/dbuchan/inputs/Pfam-A.full.uniprot.fa /home/dbuchan/outputs/hmm_closest/ ${SGE_TASK_ID}'
+echo 'python scripts/hmmer_seqs/find_closest_hmm_seq_family.py /home/dbuchan/inputs/hmm_generated_seqs.fa /home/dbuchan/inputs/Pfam-A.full.uniprot.fa /home/dbuchan/outputs/hmm_closest/ ${SGEls_TASK_ID}'
 python /home/ucbcdwb/Applications/profile_drift_new/scripts/hmmer_seqs/find_closest_hmm_seq_family.py /home/ucbcdwb/inputs/hmm_generated_seqs.fa /home/ucbcdwb/inputs/Pfam-A.full.uniprot.fa /home/ucbcdwb/Scratch/output/hmm ${SGE_TASK_ID}
 
 cd ~/
