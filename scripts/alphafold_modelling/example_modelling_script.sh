043d696cafd25887fc82d24fb96f5731042e8abe
Daniel Buchan <clone@myself.com>
2024-09-20 16:09:25 +0100

    skeleton of alphafold modelling script has run

--- scripts/alphafold_modelling/example_modelling_script.sh
+++ scripts/alphafold_modelling/example_modelling_script.sh
@@ -3,7 +3,7 @@
 #$ -e /home/ucbcdwb/Scratch/output/alpha/std.err
 
 # Request an hour of run time
-#$ -l h_rt=01:00:0
+#$ -l h_rt=200:00:0
 
 # Request 10 gigabyte of TMPDIR space (default is 10 GB - remove if cluster is diskless)
 #$ -l tmpfs=10G
@@ -16,16 +16,25 @@
 
 # Set up the job array.  In this instance we have requested 10000 tasks
 # numbered 1 to 10000.
-#$ -t 1-10000
+#$ -t 1-2
 # Set the name of the job.
+
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/share/apps/cuda-11.8/lib64/
-conda activate /cluster/project1/ProCovar/lmoffat/miniconda/envs/alphafold
+conda activate /cluster/project1/ProCovar/lmoffat/miniconda/envs/alphafold 
 cd /cluster/project1/ProCovar/colabfold-customtemplates
-bash /cluster/project1/ProCovar/colabfold-customtemplates/main_db.sh test_run.single_sequence.fa test_run.single_sequence.a3m db_test
+
+CLASS='contaminants_grew' # remove 57 leading chars
+FASTA_ID="$(tail -n +${SGE_TASK_ID} /home/dbuchan/inputs/alphafold/${CLASS}/targets | head -n 1)"
+FAMILY_ID=${FASTA_ID::-3}
+
+for i in /home/dbuchan/inputs/alphafold/${CLASS}/${FAMILY_ID}_*.a2m; do
+    # bash /cluster/project1/ProCovar/colabfold-customtemplates/main_db.sh ${FASTA_ID} $i ${CLASS}_${FAMILY_ID}
+    echo "bash /cluster/project1/ProCovar/colabfold-customtemplates/main_db.sh /home/dbuchan/inputs/alphafold/${CLASS}/${FASTA_ID} $i ${CLASS}_${FAMILY_ID}_${i:57:-4}"
+done
 
 # move the model
-cp db_test_*_unrelaxed_rank_1_model_*.pdb /home/dbuchan/alphafold_runs/
+cp ${CLASS}_${FAMILY_ID}_*_unrelaxed_rank_1_model_*.pdb /home/dbuchan/alphafold_runs/
 # tidy up
-rm -rf db_test_*_all
-rm -rf db_test_*_template
-rm -f db_test*
+rm -rf ${CLASS}_${FAMILY_ID}_*_all
+rm -rf ${CLASS}_${FAMILY_ID}_*_template
+rm -f ${CLASS}_${FAMILY_ID}*
