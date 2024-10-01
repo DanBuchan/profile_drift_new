a15f6d810ee07456d0f9081d4bd6e2aef70d17b5
Daniel Buchan <clone@myself.com>
2024-10-01 15:44:32 +0100

    collate models

--- scripts/alphafold_modelling/sum_plddts.py
+++ scripts/alphafold_modelling/sum_plddts.py
@@ -2,11 +2,11 @@ import glob
 import sys
 
 #
-# python scripts/alphafold_modelling/sum_plddts.py ./results_data/alphafold_models/*.pdb
+# python scripts/alphafold_modelling/sum_plddts.py ./results_data/alphafold_models/
 #
 
 results = {}
-for file in glob.glob(sys.argv[1]):
+for file in glob.glob(f'{sys.argv[1]}/*.pdb'):
     with open(file, "r", encoding="utf-8") as fh:
         for line in fh:
             if line.startwith("ATOM"):
