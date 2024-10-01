898e3058e0ed22db85536182bbe5bb2e54e0e64f
Daniel Buchan <clone@myself.com>
2024-10-01 16:22:32 +0100

    collate models

--- scripts/alphafold_modelling/sum_plddts.py
+++ scripts/alphafold_modelling/sum_plddts.py
@@ -10,23 +10,23 @@ for file in glob.glob(f'{sys.argv[1]}/*.pdb'):
     with open(file, "r", encoding="utf-8") as fh:
         if 'contaminants_complex_PF00106_1_428ec_unrelaxed_rank_1_model_3.pdb' not in file:
             continue
-        print(file)
+        #print(file)
         file_parts = file[len(sys.argv[1]):].split("_")
         drift_class = f'{file_parts[0]}_{file_parts[1]}'
         family = file_parts[2]
         iteration = file_parts[3]
-        print(drift_class, family, iteration)
+        #print(drift_class, family, iteration)
         plDDT_scores = {}
         for line in fh:
             if line.startswith("ATOM"):
                 res_id = line[22:26]
-                # res_id = int(res_id.replace(" ", ""))
-                print(f'_{res_id}_')
+                res_id = int(res_id.replace(" ", ""))
+                #print(f'_{res_id}_')
                 plDDT = line[61:66]
                 plDDT = float(plDDT.replace(" ", ""))
-                print(f'{plDDT}')
+                #print(f'{plDDT}')
                 plDDT_scores[res_id] = plDDT
-        print(plDDT_scores)
+        #print(plDDT_scores)
         plDDT_tot = 0
         plDDT_count = 0
         for res_id in plDDT_scores:
@@ -39,7 +39,7 @@ for file in glob.glob(f'{sys.argv[1]}/*.pdb'):
             results[drift_class][family] = {}
         
         results[drift_class][family][iteration] = ave_plDDT
-        print(results)
+        #print(results)
     # break
 
 print("drift_class,family,iteraton,mean_plDDT")
