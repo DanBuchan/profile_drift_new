b009ea8728daa08f1cedc5245ece837528a10bc8
Daniel Buchan <clone@myself.com>
2024-10-01 15:47:09 +0100

    collate models

--- scripts/alphafold_modelling/sum_plddts.py
+++ scripts/alphafold_modelling/sum_plddts.py
@@ -8,8 +8,14 @@ import sys
 results = {}
 for file in glob.glob(f'{sys.argv[1]}/*.pdb'):
     with open(file, "r", encoding="utf-8") as fh:
+        file_parts = file.split("_")
+        print(file_parts)
+        drift_class = f'{file_parts[0]}_{file_parts[1]}'
+        family = file_parts[2]
+        iteration = file_parts[3]
+
         for line in fh:
             if line.startswith("ATOM"):
                 entries = line.split()
-                print(entries)
+                # print(entries)
     break
