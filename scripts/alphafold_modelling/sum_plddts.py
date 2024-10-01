e3b74d208db9a1c2aa67209388f2509e80e66619
Daniel Buchan <clone@myself.com>
2024-10-01 15:55:01 +0100

    collate models

--- scripts/alphafold_modelling/sum_plddts.py
+++ scripts/alphafold_modelling/sum_plddts.py
@@ -18,7 +18,7 @@ for file in glob.glob(f'{sys.argv[1]}/*.pdb'):
         for line in fh:
             if line.startswith("ATOM"):
                 res_id = line[24:28]
-                print(res_id)
+                print(f'{res_id}_')
                 plDDT = line[61:66]
                 plDDT = float(plDDT.replace(" ", ""))
                 print(f'{plDDT}')
