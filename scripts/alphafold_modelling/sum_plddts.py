42c54db2d625b01adee7d95dea5a7041a6ebd64e
Daniel Buchan <clone@myself.com>
2024-10-01 16:17:29 +0100

    collate models

--- scripts/alphafold_modelling/sum_plddts.py
+++ scripts/alphafold_modelling/sum_plddts.py
@@ -15,16 +15,16 @@ for file in glob.glob(f'{sys.argv[1]}/*.pdb'):
         drift_class = f'{file_parts[0]}_{file_parts[1]}'
         family = file_parts[2]
         iteration = file_parts[3]
-        # print(drift_class, family, iteration)
+        print(drift_class, family, iteration)
         plDDT_scores = {}
         for line in fh:
             if line.startswith("ATOM"):
                 res_id = line[24:28]
                 res_id = int(res_id.replace(" ", ""))
-                # print(f'{res_id}_')
+                print(f'{res_id}_')
                 plDDT = line[61:66]
                 plDDT = float(plDDT.replace(" ", ""))
-                # print(f'{plDDT}')
+                print(f'{plDDT}')
                 plDDT_scores[res_id] = plDDT
         plDDT_tot = 0
         plDDT_count = 0
@@ -38,7 +38,7 @@ for file in glob.glob(f'{sys.argv[1]}/*.pdb'):
             results[drift_class][family] = {}
         
         results[drift_class][family][iteration] = ave_plDDT
-        # print(results)
+        print(results)
     # break
 
 print("drift_class,family,iteraton,mean_plDDT")
