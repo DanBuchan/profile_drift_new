00e6258c17406b511a92915c60341d26cd3fb0cf
Daniel Buchan <clone@myself.com>
2024-10-25 14:12:55 +0100

    things

--- scripts/psiblast_drift/calculate_contamination_distances.py
+++ scripts/psiblast_drift/calculate_contamination_distances.py
@@ -42,7 +42,7 @@ files = [query_purified, contam_purified, contam_grew, contam_complex]
 with open('/home/dbuchan/Projects/profile_drift/results_data/distance_matrix/pfam_rep_all_against_all/rand_rep_distance_matrix.npy', 'rb') as f:
     dist_matrix = np.load(f)
     dom_list = np.load(f)
-# drift_summaries = read_summaries("/home/dbuchan/Projects/profile_drift/results_data/drift/pfam_rep_psiblast_iteration_summaries/")
+drift_summaries = read_summaries("/home/dbuchan/Projects/profile_drift/results_data/drift/pfam_rep_psiblast_iteration_summaries/")
 
 # bins = list(np.arange(0.0,1.0,0.001))
 # flat_array = dist_matrix.flatten()
@@ -53,4 +53,23 @@ with open('/home/dbuchan/Projects/profile_drift/results_data/distance_matrix/pfa
 # ax.bar(edges[:-1], frq, width=np.diff(edges), edgecolor="black", align="edge")
 
 # plt.show()
-print(f'mean: {dist_matrix.mean()}, std: {dist_matrix.std()}')
+
+# print(f'mean: {dist_matrix.mean()}, std: {dist_matrix.std()}')
+
+total_dist = 0
+total_comparisions =0
+for file in files:
+    pfam_set = read_list(summaries_location, file)
+    pfam_id_set = set()
+    for pfam in pfam_set:
+        pfam_id_set = set()
+        if pfam in drift_summaries:
+            for iteration in drift_summaries[pfam]:
+                for hit_pfam in drift_summaries[pfam][iteration]:
+                    if pfam not in hit_pfam:
+                        pfam_id_set.add(hit_pfam)
+        query_idx = dom_list.index(pfam)
+        for hit_pfam in pfam_id_set:
+            hit_idx = dom_list.index(hit_pfam)
+            print(dist_matrix[query_idx,hit_idx])
+            exit
