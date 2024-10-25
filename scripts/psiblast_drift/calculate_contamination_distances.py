277a64ba2f74367100e151d569319c01a15bc3ee
Daniel Buchan <clone@myself.com>
2024-10-25 12:16:15 +0100

    things

--- scripts/psiblast_drift/calculate_contamination_distances.py
+++ scripts/psiblast_drift/calculate_contamination_distances.py
@@ -38,7 +38,7 @@ contam_grew = "set_where_contaminants_grew.txt"
 contam_complex = "set_with_complex_contamination_behaviours.txt"
 files = [query_purified, contam_purified, contam_grew, contam_complex]
 
-dist_matrix = np.load("/home/dbuchan/Projects/profile_drift_new/results_data/distance_matrix/pfam_rep_all_against_all/rand_rep_distance_matrix.npy")
+dist_matrix = np.load("/home/dbuchan/Projects/profile_drift/results_data/distance_matrix/pfam_rep_all_against_all/rand_rep_distance_matrix.npy")
 drift_summaries = read_summaries("/home/dbuchan/Projects/profile_drift/results_data/drift/pfam_rep_psiblast_iteration_summaries/")
 
 print(dist_matrix)
